2dbdbbb4c690766c6f7ac6bd761d1ce4497c7432
egeerdes <49660943+egeerdes@users.noreply.github.com>
2026-03-26 10:55:09 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import mdPlugin from 'vite-plugin-markdown'
 
 export default defineConfig({
-	plugins: [react(), cloudflare()],
+	plugins: [react(), cloudflare(),mdPlugin({mode:['html','toc']})]
 });
