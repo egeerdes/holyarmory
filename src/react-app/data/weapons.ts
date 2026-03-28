55236536d1fb86acc0b1c5c2a1249dd5e2604e95
egeerdes <49660943+egeerdes@users.noreply.github.com>
2026-03-28 11:36:44 -0700

    Update weapons.ts

--- src/react-app/data/weapons.ts
+++ src/react-app/data/weapons.ts
@@ -13,3 +13,18 @@ export interface weapons {
   lie: LieTruthPair[];
   
 }
+
+const weaponRegistry: weapons[] = [
+  {
+    id: 0,
+    title: "prayer against lust",
+    category: "lust",
+    blurb: "This is a blurb about the spirit of lust.",
+    emergencyPrayer: "I stand against you, spirit of lust, under the authority of Jesus Christ. I rebuke you for tempting me. I renounce our identity and refuse to drink your stolen water. I will not eat of your bread, for the bread that sustains me is through the blood and body of Jesus Christ. Your bonds are broken, my debt is paid. Be gone from me before I get my Father involved."
+    lie: [{lie: "This is the most love you'll ever get.", truth: "There is no greater love than to lay down one's life for one's friends."}]
+  },
+  {
+    
+  }
+    
+  ]
